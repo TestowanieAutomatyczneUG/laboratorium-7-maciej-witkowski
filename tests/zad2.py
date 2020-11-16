bd34ff5ded31c87b5a5024d9a3655faea37e9915
maciej-witkowski <maciej.jan.witkowski@gmail.com>
2020-11-16 22:42:17 +0100

    zad2 - parameterized: the class

--- tests/zad2.py
+++ tests/zad2.py
@@ -27,5 +27,33 @@ class PasswordParameterizedPackage(unittest.TestCase):
         self.password = Password()
 
 
+@parameterized_class(('pwd', 'exp'), [
+    ("", False),
+    ("AaBbCc123#", True),
+    ("ABc12#", False),
+    ("aabbcc123#", False),
+    ("AaBbCc#&$", False),
+    ("AaBbCc123", False),
+])
+class PasswordParameterizedPackage2Tests(unittest.TestCase):
+    def test_second_parameterized_tests(self):
+        self.assertEqual(self.password.ValidPassword(self.pwd), self.exp)
+
+    def setUp(self):
+        self.password = Password()
+
+
+@parameterized_class(('inp', 'exp'), [
+    (2115, TypeError),
+    ([], TypeError),
+])
+class PasswordParameterizedPackage2Raises(unittest.TestCase):
+    def test_second_parameterized_raises(self):
+        self.assertRaises(self.exp, self.password.ValidPassword, self.inp)
+
+    def setUp(self):
+        self.password = Password()
+
+
 if __name__ == '__main__':
     unittest.main()
